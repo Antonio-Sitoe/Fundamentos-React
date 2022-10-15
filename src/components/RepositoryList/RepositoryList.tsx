b4893a515ea7bc1d9a73aaf7fd935e2beb1390ea
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2022-10-15 23:22:15 +0200

    project init

---
+++ src/components/RepositoryList/RepositoryList.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const RepositoryList = () => {
+  return (
+    <div>
+      <h1>Lista de repositorio</h1>
+      <ul>
+        <li>
+          <strong>Unform</strong>
+          <p>Forms in React</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default RepositoryList;
